e12bec0db9291a3ec4b0f508d3189f9f0b064b56
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2024-04-03 20:45:46 -0300

    Add files via upload

--- app.R
+++ app.R
@@ -3,25 +3,25 @@ library(bslib)
 library(dplyr)
 library(ggplot2)
 library(ggExtra)
+library(dados)
 
-penguins_csv <- "https://raw.githubusercontent.com/jcheng5/simplepenguins.R/main/penguins.csv"
-
-df <- readr::read_csv(penguins_csv)
+df <- dados::dados_starwars
 # Find subset of columns that are suitable for scatter plot
-df_num <- df |> select(where(is.numeric), -Year)
+df_num <- df |> select(where(is.numeric)) 
+df_num <- df_num[1:10 , ]
 
 ui <- page_sidebar(
   sidebar = sidebar(
     varSelectInput("xvar", "X variable", df_num, selected = "Bill Length (mm)"),
     varSelectInput("yvar", "Y variable", df_num, selected = "Bill Depth (mm)"),
     checkboxGroupInput(
-      "species", "Filter by species",
-      choices = unique(df$Species), 
-      selected = unique(df$Species)
+      "nome", "Filter by nomes",
+      choices = unique(df$nome), 
+      selected = unique(df$nome)
     ),
     hr(), # Add a horizontal rule
-    checkboxInput("by_species", "Show species", TRUE),
-    checkboxInput("show_margins", "Show marginal plots", TRUE),
+    checkboxInput("por_nomes", "Mostrar nomes", TRUE),
+    checkboxInput("mostrar_margins", "Mostrar marginal plots", TRUE),
     checkboxInput("smooth", "Add smoother"),
   ),
   plotOutput("scatter")
@@ -29,22 +29,22 @@ ui <- page_sidebar(
 
 server <- function(input, output, session) {
   subsetted <- reactive({
-    req(input$species)
-    df |> filter(Species %in% input$species)
+    req(input$nome)
+    df |> filter(nome %in% input$nome)
   })
 
   output$scatter <- renderPlot({
     p <- ggplot(subsetted(), aes(!!input$xvar, !!input$yvar)) + list(
       theme(legend.position = "bottom"),
-      if (input$by_species) aes(color = Species),
+      if (input$por_nomes) aes(color = nome),
       geom_point(),
       if (input$smooth) geom_smooth()
     )
 
     if (input$show_margins) {
-      margin_type <- if (input$by_species) "density" else "histogram"
+      margin_type <- if (input$por_nomes) "density" else "histogram"
       p <- ggExtra::ggMarginal(p, type = margin_type, margins = "both",
-        size = 8, groupColour = input$by_species, groupFill = input$by_species)
+        size = 8, groupColour = input$por_nomes, groupFill = input$por_nomes)
     }
 
     p
