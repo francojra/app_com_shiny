479564821216edfe60415ff136dd8bde5ece99d4
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2024-04-05 23:28:51 -0300

    Update app.R

--- app.R
+++ app.R
@@ -1,57 +1,63 @@
+# Pacotes necessários
+
 library(shiny)
 library(bslib)
-library(tidyverse)
+library(dplyr)
+library(ggplot2)
 library(ggExtra)
-library(dados)
 
-df <- dados::dados_starwars
+# Dados
 
-df <- df |>
-  drop_na()
-df$genero <- as.factor(df$genero)
+penguins_csv <- "https://raw.githubusercontent.com/jcheng5/simplepenguins.R/main/penguins.csv"
 
+df <- readr::read_csv(penguins_csv)
 # Find subset of columns that are suitable for scatter plot
-df_num <- df |> select(where(is.numeric), -ano_nascimento) 
+df_num <- df |> select(where(is.numeric), -Year)
+
+# Seção UI (User Interface) para design de interação
 
 ui <- page_sidebar(
   sidebar = sidebar(
-    varSelectInput("xvar", "X variable", df_num, selected = "altura"),
-    varSelectInput("yvar", "Y variable", df_num, selected = "massa"),
+    varSelectInput("xvar", "X variable", df_num, selected = "Bill Length (mm)"),
+    varSelectInput("yvar", "Y variable", df_num, selected = "Bill Depth (mm)"),
     checkboxGroupInput(
-      "genero", "Filter by gênero",
-      choices = unique(df$genero), 
-      selected = unique(df$genero)
+      "species", "Filter by species",
+      choices = unique(df$Species), 
+      selected = unique(df$Species)
     ),
     hr(), # Add a horizontal rule
-    checkboxInput("por_genero", "Mostrar gêneros", TRUE),
-    checkboxInput("mostrar_margins", "Mostrar marginal plots", TRUE),
+    checkboxInput("by_species", "Show species", TRUE),
+    checkboxInput("show_margins", "Show marginal plots", TRUE),
     checkboxInput("smooth", "Add smoother"),
   ),
   plotOutput("scatter")
 )
 
+# Seção server para executar as funções 
+
 server <- function(input, output, session) {
   subsetted <- reactive({
-    req(input$genero)
-    df |> filter(genero %in% input$genero)
+    req(input$species)
+    df |> filter(Species %in% input$species)
   })
 
   output$scatter <- renderPlot({
     p <- ggplot(subsetted(), aes(!!input$xvar, !!input$yvar)) + list(
       theme(legend.position = "bottom"),
-      if (input$por_genero) aes(color = genero),
+      if (input$by_species) aes(color = Species),
       geom_point(),
       if (input$smooth) geom_smooth()
     )
 
     if (input$show_margins) {
-      margin_type <- if (input$por_genero) "density" else "histogram"
+      margin_type <- if (input$by_species) "density" else "histogram"
       p <- ggExtra::ggMarginal(p, type = margin_type, margins = "both",
-        size = 8, groupColour = input$por_genero, groupFill = input$por_genero)
+        size = 8, groupColour = input$by_species, groupFill = input$by_species)
     }
 
     p
   }, res = 100)
 }
 
+# Executar aplicativo
 shinyApp(ui, server)
